d452f8e3321679c51326a6d0a95db8b2864009de
stanislavbabenko <sbabenk1@jh.edu>
2024-04-26 20:20:41 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-// src/App.js
 import React from 'react';
-import LandingPageComponent from './LandingPageComponent';
 
 function App() {
   return (
     <div>
-	<LandingPageComponent />
     </div>
   );
 }
